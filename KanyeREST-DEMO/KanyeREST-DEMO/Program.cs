c8b133fed1c956fecf88b1106a5bad50deb0eb87
jdw <johndward01@gmail.com>
2021-10-27 09:13:46 -0500

    update

---
+++ KanyeREST-DEMO/KanyeREST-DEMO/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Net.Http;
+
+namespace KanyeREST_DEMO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var kanyeResponse = client.GetStringAsync("https://api.kanye.rest").Result;
+                var ronResponse = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+                Console.WriteLine($" Kanye West  - {kanyeQuote}");
+                Console.WriteLine();
+                Console.WriteLine($" Ron Swanson - {ronQuote}");
+            }
+            
+        }
+    }
+}
