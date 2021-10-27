f157cdae064ea977985a4259c3b161b951659409
jdw <johndward01@gmail.com>
2021-10-26 19:36:03 -0500

    update

--- OpenWeatherMapAPI_Exercise/OpenWeatherMap_Exercise/Program.cs
+++ OpenWeatherMapAPI_Exercise/OpenWeatherMap_Exercise/Program.cs
@@ -16,30 +16,25 @@ namespace OpenWeatherMap_Exercise
             while (true)
             {
                 Console.WriteLine();
-
                 Console.Write("Please enter the city name: ");
                 var city_name = Console.ReadLine();
-
                 Console.WriteLine();
 
                 var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_Key}";
-
                 var response = client.GetStringAsync(weatherURL).Result;
-
                 var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
                 Console.WriteLine(formattedResponse);
-
                 Console.WriteLine();
 
                 Console.WriteLine("Would you like to choose a different city?");
                 var userInput = Console.ReadLine();
-
                 Console.WriteLine();
 
                 if (userInput.ToLower().Trim() == "no")
                 {
                     break;
                 }
+
             }
             
 
