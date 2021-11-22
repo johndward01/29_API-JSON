fe75af145d797d7d0d238177f68b6040cdf560b3
John Ward <59837086+johndward01@users.noreply.github.com>
2021-11-22 13:57:43 -0600

    Update Program.cs

--- Deserializing-OpenWeatherMap-Json/Deserializing-OpenWeatherMap-Json/Program.cs
+++ Deserializing-OpenWeatherMap-Json/Deserializing-OpenWeatherMap-Json/Program.cs
@@ -13,13 +13,15 @@ namespace OpenWeatherMap_Exercise
         {
             var client = new HttpClient();
             var jsonRootObject = new Json_Root();
+            
             Console.Write("Please enter your API Key: ");
-
             var api_Key = Console.ReadLine();
 
             Console.WriteLine();
+            
             Console.Write("Please enter the city name: ");
             var city_name = Console.ReadLine();
+            
             Console.WriteLine();
 
             var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_Key}";
@@ -27,17 +29,15 @@ namespace OpenWeatherMap_Exercise
             var weatherJson = JObject.Parse(response).GetValue("weather").ToString();
             jsonRootObject.WeatherList = JsonConvert.DeserializeObject<List<Weather>>(weatherJson);
 
-            jsonRootObject.WeatherList.ForEach(x => PrintWeather(x));
-
-
-            static void PrintWeather(Weather w)
-            {
-                Console.WriteLine(w.description);
-                Console.WriteLine(w.icon);
-                Console.WriteLine(w.id);
-                Console.WriteLine(w.main);
-            }
-
+            jsonRootObject.WeatherList.ForEach(x => PrintWeather(x));            
+        }
+        
+        static void PrintWeather(Weather w)
+        {
+            Console.WriteLine(w.description);
+            Console.WriteLine(w.icon);
+            Console.WriteLine(w.id);
+            Console.WriteLine(w.main);
         }
     }
 }
